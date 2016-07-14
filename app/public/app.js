0e36dd8791fdff33e7cd7adea04876f3f7c5960f
HJH-mac <1041536113@stu.nkmu.edu.tw>
2016-07-14 19:09:20 +0800

    edit

---
+++ app/public/app.js
@@ -0,0 +1,34 @@
+(function(){
+    'use strict';
+
+    angular
+        .module('tabsApp')
+        .controller('tabsCtrl', tabsCtrl)
+
+    function tabsCtrl(Dependencies){
+        var own = this;
+        
+
+    }
+
+}());
+
+(function(){
+    'use strict';
+
+    angular
+        .module('Module')
+        .controller('ControllerCtrl', ControllerCtrl)
+
+    function ControllerCtrl(Dependencies){
+        var vm = this;
+        
+        init();
+
+        function init(){
+        }
+
+    }
+
+}());
+
